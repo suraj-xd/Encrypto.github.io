836a8caee0e54a5a1f923ae456d7b0b4df2873f9
Suraj Gaud <75672977+suraj-xd@users.noreply.github.com>
2021-06-08 04:45:08 -0700

    Update index.js

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ var copybutton1 = document.getElementById('copybutton1');
 var copybutton2 = document.getElementById('copybutton2');
 var copytext = document.getElementById('copytext');
 function generator() {
+            
             var IncriptedText = document.getElementById('IncriptedText');
             var Keyvalue = document.getElementById('keyvalue');
 
@@ -26,79 +27,89 @@ function generator() {
 
             let ans="";
             let s = Generateinput.value;
-            s = s.toUpperCase();
-            var randomnum = String((Math.floor(Math.random() * 9012)) + 999);
-            for(let i=0;i<4;i++){
-                ans+=randomnum[i];
+            if(s!=""){
+              s = s.toUpperCase();
+              var randomnum = String((Math.floor(Math.random() * 9012)) + 999);
+              for(let i=0;i<4;i++){
+                  ans+=randomnum[i];
+              }
+              for(let i=0;i<Generateinput.value.length;i++){
+                  if (s[i] == " ") {
+                    ans += "32";
+                  } else {
+                    var char1 = s[i];
+                    ans += char1.charCodeAt() - 26;
+                  }
+              }
+              let answer = "";
+              for (let i = 0; i < ans.length; i += 2) {
+                  let num1 = ans[i];
+                  let num2 = ans[i + 1];
+                  var num = num1 + num2;
+                  var bum = parseInt(num);
+                  if (bum == 32) {
+                    answer += " ";
+                  } else {
+                    answer += String.fromCharCode(bum + "26");
+                  }
+              }
+              
+              IncriptedText.innerHTML = answer;
+              Keyvalue.innerHTML = randomnum;
+              let showpopup = document.querySelector('.showpopop');
+              showpopup.style.visibility = 'visible';
+            } else {
+              alert("Enter Some Text!");
             }
-            for(let i=0;i<Generateinput.value.length;i++){
-                if (s[i] == " ") {
-                  ans += "32";
-                } else {
-                  var char1 = s[i];
-                  ans += char1.charCodeAt() - 26;
-                }
-            }
-            let answer = "";
-            for (let i = 0; i < ans.length; i += 2) {
-                let num1 = ans[i];
-                let num2 = ans[i + 1];
-                var num = num1 + num2;
-                var bum = parseInt(num);
-                if (bum == 32) {
-                  answer += " ";
-                } else {
-                  answer += String.fromCharCode(bum + "26");
-                }
-            }
-            IncriptedText.innerHTML = answer;
-            Keyvalue.innerHTML = randomnum;
-            let showpopup = document.querySelector('.showpopop');
-            showpopup.style.visibility = 'visible';
 }
 var inputtxt = document.querySelector('.inputtxt');
 var keyinput = document.querySelector('.keyinput');
 // var readtext = document.querySelector('.readtext');
-
 function readtext() {
             let inputtxt = document.querySelector(".inputtxt");
             let keyinput = document.querySelector(".keyinput");
 
             let ans = inputtxt.value;
-            var answer="";
-            for (let i = 0; i < 2; i++) {
-                let char1 = (ans[i]);
-                answer += (char1.charCodeAt() - 26) / 100;
-            }
-            for (let i = 2; i < ans.length; i++) {
-                let char1 = (ans[i]);
-                answer += (char1.charCodeAt() - 26)/100;
-            }
-            var keycode="";
-            for(let i=0;i<4;i++){
-                keycode+=answer[i];
-            }
-            if(keyinput.value==keycode){
-                var answercode="";
-                for (let i = 4; i < answer.length-3; i += 2) {
+            let genkey =keyinput.value;
+            if(ans=="" || genkey==""){
+                  console.log("yes");
+                  alert("Enter the Details!")
+            }else{
+                var answer = "";
+                for (let i = 0; i < 2; i++) {
+                  let char1 = (ans[i]);
+                  answer += (char1.charCodeAt() - 26) / 100;
+                }
+                for (let i = 2; i < ans.length; i++) {
+                  let char1 = (ans[i]);
+                  answer += (char1.charCodeAt() - 26) / 100;
+                }
+                var keycode = "";
+                for (let i = 0; i < 4; i++) {
+                  keycode += answer[i];
+                }
+                if (keyinput.value == keycode) {
+                  var answercode = "";
+                  for (let i = 4; i < answer.length - 3; i += 2) {
                     let num1 = answer[i];
                     let num2 = answer[i + 1];
                     var num = num1 + num2;
                     var bum = parseInt(num) + 26;
-                    console.log(bum);
                     if (bum == 26) {
-                        answer += " ";
+                      answer += " ";
                     } else {
                       answercode += String.fromCharCode(bum);
                     }
+                  }
+                  // console.log(answercode);
+                  var IncriptedCode = document.getElementById("IncriptedCode");
+                  IncriptedCode.innerText = answercode;
+                  
+
                 }
-                // console.log(answercode);
-                var IncriptedCode = document.getElementById("IncriptedCode");
-                IncriptedCode.innerText = answercode;
-                
-            }else{
-                console.log("No")
-            }
+              }
+              
+            
             
 }
         
